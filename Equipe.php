0c989677a54dedf837d873e0a834c2771f39004c
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-06-19 22:04:19 +0200

    MAJ COMMENTAIRE DE LA CLASS EQUIPE

--- Equipe.php
+++ Equipe.php
@@ -1,47 +1,50 @@
 <?php
 
-class Equipe {
-    private $nom;
-    private $pays;
-    private $dateDeCreation;
-    private $transferts = [];
 
-    public function __construct($nom, $pays, $dateDeCreation) {
-        $this->nom = $nom;
-        $this->pays = $pays;
-        $this->dateDeCreation = $dateDeCreation;
-        $this->pays->ajouterEquipe($this);
+class Equipe {                                                                                     // CREE UNE CLASS APPELE "EQUIPE" ET VONT STOCKER LES ATTRIBUTS DANS LA CLASS "EQUIPE"
+    private $nom;                                                                                  // LE NOM DE L'EQUIPE
+    private $pays;                                                                                 // LE PAYS DE L'EQUIPE
+    private $dateDeCreation;                                                                       // LA DATE DE CRÉATION DE L'EUIPE
+    private $transferts = [];                                                                      // UN TABLEAU VIDE POUR GARDER LES INFORMATIONS DE TRANSFERTS D EQUIPE
+
+                                                                 
+    public function __construct($nom, $pays, $dateDeCreation) {                                    // UNE FONCTION QUI ASSIGNE LES VALEURS A EQUIPE
+
+        $this->nom = $nom;                                                                         // NOM  DE L'EQUIPE
+        $this->pays = $pays;                                                                       // PAYS DE L'EQUIPE
+        $this->dateDeCreation = $dateDeCreation;                                                   // DATE DE CRÉATION DE L'EQUIPE
+        $this->pays->ajouterEquipe($this);                                                         // AJOUTE LES INFORMATIONS  EQUIPE AU TABLEAU DES EQUIPES DU PAYS
     }
 
-    public function ajouterTransfert($transfert) {
-        $this->transferts[] = $transfert;
+    public function ajouterTransfert($transfert) {                                                 // UNE FONCTION QUI AJOUTE UN TRANSFERT A LA LISTE DES TRANSFERTS
+        $this->transferts[] = $transfert;                                                          // AJOUTE LES INFORMATIONS DE TRANSFERT A LA LISTE
+    }
+                    
+    public function getNom() {                                                                     // UNE FONCTION QUI RETOURNE LE NOM DE L'EQUIPE
+        return $this->nom;                                                                         // RETOURNE LE NOM
     }
 
-    public function getNom() {
-        return $this->nom;
+    public function getPays() {                                                                    // UNE FONCTION QUI RETOURNE LE PAYS DE L'ÉEUIPE
+        return $this->pays;                                                                        // RETOURNE LE PAYS
     }
 
-    public function getPays() {
-        return $this->pays;
-    }
+    public function afficherLesCartesDeux() {                                                      // UNE FONCTION QUI AFFICHE LES INFORMATIONS DE L'EQUIPE ET SES TRANSFERTS
+        
+        echo "<div style='width: 200px; background-color: #6680af ; color: white; 
+         border-radius: 20px; text-align: center;padding: 10px;'>";                                // CREE UNE DIV AVEC UN STYLE
 
-    public function afficherLesCartesDeux() {
+        echo "<h3>{$this->nom}</h3>";                                                              // AFFICHE LE NOM DE L'EQUIPE
+                              
+        echo "<p>{$this->pays->getNom()} - {$this->dateDeCreation}</p>";                           // AFFICHE LE NOM DU PAYS ET LA DATE DE CREATION DE L'EQUIPE
 
-        echo "<div style='width: 200px; background-color: #6680af ; color: white; border-radius: 20px; text-align: center;padding: 10px;'>";
+        foreach ($this->transferts as $transfert) {                                                // POUR CHAQUE TRANSFERT DANS LA LISTE DES TRANSFERTS
+            $joueur = $transfert->getJoueur();                                                     // RÉCUPÈRE LE JOUEUR DU TRANSFERT
+            $dateDebut = new DateTime($transfert->getDateDebut());                                 // RÉCUPÈRE LA DATE DE DÉBUT DU TRANSFERT
 
-        echo "<h3>{$this->nom}</h3>";
-
-        echo "<p>{$this->pays->getNom()} - {$this->dateDeCreation}</p>";
-
-        foreach ($this->transferts as $transfert) {
-
-            $joueur = $transfert->getJoueur();
-
-            $dateDebut = new DateTime($transfert->getDateDebut());
-
-            echo "<p>{$joueur->getPrenom()} {$joueur->getNom()} ({$dateDebut->format('Y')})</p>";
+            echo "<p>{$joueur->getPrenom()} {$joueur->getNom()} ({$dateDebut->format('Y')})</p>";  // AFFICHE LE PRENOM ET LE NOM DU JOUEUR ET L'ANNEE DU TRANSFERT
         }
-        echo "</div>";
+
+        echo "</div>";                                                                             // FERME LA DIV
     }
 }
 
