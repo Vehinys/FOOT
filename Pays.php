61b014a91bbf6826c81eec50e948e10a0ef50c9a
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-06-19 22:21:37 +0200

    MAJ COMMENTAIRE PAYS

--- Pays.php
+++ Pays.php
@@ -1,34 +1,41 @@
 <?php
 
-class Pays {
-    private $nom;
-    private $nationalite;
-    private $equipes = [];
 
-    public function __construct($nom, $nationalite) {
-        $this->nom = $nom;
-        $this->nationalite = $nationalite;
+class Pays {                                                                         // CREE UNE CLASS APPELE "PAYS" ET VONT STOCKER LES ATTRIBUTS DANS LA CLASS "PAYS"
+
+    private $nom;                                                                    // LE NOM DU PAYS
+    private $nationalite;                                                            // LA NATIONALITÉ DU PAYS
+    private $equipes = [];                                                           // UN TABLEAU VIDE POUR GARDER LES INFORMATIONS D EQUIPE
+
+    public function __construct($nom, $nationalite) {                                // CONSTRUCTEUR DE LA CLASSE PAYS
+        $this->nom = $nom;                                                           // INITIALISE LE NOM DU PAYS
+        $this->nationalite = $nationalite;                                           // INITIALISE LA NATIONALITÉ DU PAYS
     }
 
-    public function ajouterEquipe($equipe) {
-        $this->equipes[] = $equipe;
+
+    public function ajouterEquipe($equipe) {                                         // METHODE POUR AJOUTER UNE EQUIPE À LA LISTE
+        $this->equipes[] = $equipe;                                                  // AJOUTE UNE EQUIPE À LA LISTE DES EQUIPES DU PAYS
     }
 
-    public function getNom() {
-        return $this->nom;
+    public function getNom() {                                                       // METHODE POUR OBTENIR LE NOM DU PAYS
+        return $this->nom;                                                           // RETOURNE LE NOM DU PAYS
     }
 
-    public function afficherLesCartesUne() {
+    public function afficherLesCartesUne() {                                         // METHODE POUR AFFICHER LES CARTES DES EQUIPES DU PAYS
+  
+        echo "<div style='width: 200px; background-color: #bc6766 ; color: white; 
+        border-radius: 20px; text-align: center;padding: 10px;'>";                   // CREE UNE DIV AVEC UN STYLE
+        
+        echo "<h3>{$this->nom}</h3>";                                                // AFFICHE LE NOM DU PAYS COMME TITRE
 
-        echo "<div style='width: 200px; background-color: #bc6766 ; color: white; border-radius: 20px; text-align: center;padding: 10px;'>";
-
-        echo "<h3>{$this->nom}</h3>";
-
-        foreach ($this->equipes as $equipe) {
-
-            echo "<p>{$equipe->getNom()}</p>";
+        foreach ($this->equipes as $equipe) {                                        // POUR CHAQUE EQUIPE DANS LA LISTE DES EQUIPES DU PAYS
+            
+            echo "<p>{$equipe->getNom()}</p>";                                       // AFFICHE LE NOM DE L'EQUIPE
         }
-        echo "</div>";
+
+        
+        echo "</div>";                                                               // FERME LA DIV
     }
-}
-?>
+}                                                                                    
+
+
